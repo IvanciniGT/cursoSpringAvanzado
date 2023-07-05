79639d5536230181896ff0ee0ffc2b56be6aad0f
Ivan Osuna <ivan.osuna.ayuste@gmail.com>
2023-07-05 13:32:42 +0200

    Commit

--- animalitos.servicio/src/test/java/com/curso/animalitos/servicio/AplicacionParaPruebas.java
+++ animalitos.servicio/src/test/java/com/curso/animalitos/servicio/AplicacionParaPruebas.java
@@ -1,14 +1,17 @@
 package com.curso.animalitos.servicio;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 // Esta anotación de indica a Spring, que busque entidades, componentes, configuraciones.. de
 // to do   en el mismo paquete en el que estoy y sus subpaquetes
 // De lo que haya en tiempo de ejecuci`ón en el classpath
 // Esta anotación, que nos da SpringBoot, es una anotación que reemplaza a 3 anotaciones que usábamos antes:
-// @ComponentScan("paquete")
-// @EntityScan(basePackages = {"paquete1", "paquete2"})
-// @EnableJpaRepositories(basePackages = {"paquete1", "paquete2"})
+@ComponentScan("com.curso.animalitos")
+@EntityScan(basePackages = {"com.curso.animalitos"})
+@EnableJpaRepositories(basePackages = {"com.curso.animalitos"})
 public class AplicacionParaPruebas {
 }
